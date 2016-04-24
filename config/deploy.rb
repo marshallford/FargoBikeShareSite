b709556f66b467060b5a289ae4826aed19738e8c
marshallford <inbox@marshallford.me>
2016-04-24 16:34:40 -0500

    cleaning up mina options

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,23 +9,12 @@ set :deploy_to, '/sites/fargobikesharesite'
 set :repository, '/home/git/fargobikesharesite.git'
 set :branch, 'master'
 
-# Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
-# They will be linked in the 'deploy:link_shared_paths' step.
 set :shared_paths, ['log']
 
-# This task is the environment that is loaded for most commands, such as
-# `mina deploy` or `mina rake`.
 task :environment do
-  queue %{
-echo "-----> Loading environment"
-#{echo_cmd %[source ~/.bashrc]}
-}
   invoke :'rbenv:load'
 end
 
-# Put any custom mkdir's in here for when `mina setup` is ran.
-# For Rails apps, we'll make some of the shared paths that are shared between
-# all releases.
 task :setup => :environment do
   queue! %[mkdir -p "#{deploy_to}/shared/log"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/log"]
